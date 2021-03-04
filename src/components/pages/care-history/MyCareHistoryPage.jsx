ec8641ee80d7997554230abe0659b151dfada6b9
Rhone Lachner <fernandclay@gmail.com>
2021-03-04 15:56:29 -0800

    updated exports on all pages

---
+++ src/components/pages/care-history/MyCareHistoryPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MyCareHistoryPage() {
+    return (
+        <div>
+            Hello I am my care history.
+        </div>
+    )
+}
+
+export default MyCareHistoryPage
+

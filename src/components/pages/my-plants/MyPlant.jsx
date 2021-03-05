b506f1a3b43e83453d2044891ecb78a40acae98f
jeffIrvine <jirvine1212@gmail.com>
2021-03-04 16:16:11 -0800

    built my plant item and added snapshot testing

---
+++ src/components/pages/my-plants/MyPlant.jsx
@@ -0,0 +1,26 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+
+
+// const MyPlant = ({ name, image, handleDelete }) => (
+//   <div>
+//   <figure>
+//     <img src={image} alt={name}/>
+
+//     <figcaption>
+//       <p>{name}</p>
+//     </figcaption>
+    
+//   </figure>
+//   <button onClick={handleDelete}>`Remove ${name} from your collection.`</button>
+//   </div>
+
+// );
+
+// MyPlant.propTypes = {
+//   name: PropTypes.string.isRequired,
+//   image:PropTypes.string.isRequired,
+//   onClick:PropTypes.func.isRequired
+// };
+
+// export default MyPlant;

cc4fb3b8105396a023617432ccbdc3e0b7727df2
Rhone Lachner <fernandclay@gmail.com>
2021-03-04 16:56:31 -0800

    updated routing pre-snapshot update

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,29 @@
+/* eslint-disable max-len */
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+import HomePage from '../../components/pages/home/HomePage';
+import DetailsPage from '../../components/pages/details/DetailsPage';
+import MyPlantsPage from '../pages/my-plants/MyPlantsPage';
+import SearchPage from '../pages/search/SearchPage';
+import MyCareHistoryPage from '../pages/care-history/MyCareHistoryPage';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/details" component={DetailsPage} />
+          <Route exact path="/myPlants" component={MyPlantsPage} />
+          <Route exact path="/search" component={SearchPage} />
+          <Route exact path="/myCareHistoryPage" component={MyCareHistoryPage} />
+        </Switch>
+      </Router>
+    </>
+  );
 }

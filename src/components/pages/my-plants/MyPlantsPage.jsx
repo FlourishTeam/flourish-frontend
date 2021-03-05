feb8802bafd20d29bb67dac6c4fad64f2d7d65e4
jeffIrvine <jirvine1212@gmail.com>
2021-03-04 18:23:23 -0800

    end of day building plant page

---
+++ src/components/pages/my-plants/MyPlantsPage.jsx
@@ -0,0 +1,18 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import MyPlant from './MyPlant';
+// import MyPlantList from './MyPlantsList';
+// import MySearch from './my-search/MySearch';
+
+// const MyPlantsPage = () => {
+  
+
+//   return (
+//     <MyPlantList 
+    
+//     />
+//   )
+// } 
+
+// export default MyPlantsPage;
+

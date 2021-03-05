9f47f0c6ec0183105fe91382c66e0ab360c1de2d
Jena Boehm <68626277+jena-boehm@users.noreply.github.com>
2021-03-05 14:32:40 -0800

    Auth/card 4/signup login (#18)
    * add services and state folders
    
    * add AuthContext.js
    
    * Add AuthForm.jsx
    
    * add Signup.jsx, Login.jsx, PrivateRoute.jsx

--- src/state/AuthContext.js
+++ src/state/AuthContext.js
@@ -5,73 +5,73 @@ import { getVerify, postLogin, postSignup } from '../services/auth';
 const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
-    const history = useHistory();
-    const [session, setSession] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    const isAuthenticated = !!session;
+  const history = useHistory();
+  const [session, setSession] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const isAuthenticated = !!session;
 
-    useEffect(() => {
-        getVerify()
-            .then(user => setSession(user))
-            .catch(() => console.log('user not logged in'))
-            .finally(() => setLoading(false));
-    }, []);
+  useEffect(() => {
+    getVerify()
+      .then(user => setSession(user))
+      .catch(() => console.log('user not logged in'))
+      .finally(() => setLoading(false));
+  }, []);
 
-    const signup = (email, password) => {
-        return postSignup(email, password)
-            .then(user => setSession(user))
-            .then(() => history.push('/'))
-            .catch(err => setError(err));
-    };
+  const signup = (email, password) => {
+    return postSignup(email, password)
+      .then(user => setSession(user))
+      .then(() => history.push('/'))
+      .catch(err => setError(err));
+  };
 
-    const login = (email, password) => {
-        return postLogin(email, password)
-            .then(user => setSession(user))
-            .then(() => history.push('/'))
-            .catch(err => setError(err));
-    };
+  const login = (email, password) => {
+    return postLogin(email, password)
+      .then(user => setSession(user))
+      .then(() => history.push('/'))
+      .catch(err => setError(err));
+  };
 
-    return (
-        <AuthContext.Provider value={{
-            session,
-            loading,
-            error,
-            isAuthenticated,
-            signup,
-            login
-        }}>{children}</AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={{
+      session,
+      loading,
+      error,
+      isAuthenticated,
+      signup,
+      login
+    }}>{children}</AuthContext.Provider>
+  );
 };
 
 export const useSession = () => {
-    const { session } = useContext(AuthContext);
-    return session;
+  const { session } = useContext(AuthContext);
+  return session;
 };
 
 export const useAuthLoading = () => {
-    const { loading } = useContext(AuthContext);
-    return loading;
+  const { loading } = useContext(AuthContext);
+  return loading;
 };
 
 export const useAuthError = () => {
-    const { error } = useContext(AuthContext);
-    return error;
+  const { error } = useContext(AuthContext);
+  return error;
 };
 
 export const useIsAuthenticated = () => {
-    const { isAuthenticated } = useContext(AuthContext);
-    return isAuthenticated;
+  const { isAuthenticated } = useContext(AuthContext);
+  return isAuthenticated;
 };
 
 export const useSignup = () => {
-    const { signup } = useContext(AuthContext);
-    return signup;
+  const { signup } = useContext(AuthContext);
+  return signup;
 };
 
 export const useLogin = () => {
-    const { login } = useContext(AuthContext);
-    return login;
+  const { login } = useContext(AuthContext);
+  return login;
 };
 
 

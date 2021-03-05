4dc1f77aefe871cf78b262d978510da8f6fe37a9
Jena Boehm <boehmjena@gmail.com>
2021-03-05 15:37:45 -0800

    add basic popup

--- src/components/reusable/header/Header.jsx
+++ src/components/reusable/header/Header.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import styles from './styles/Header.css';
 import flourishIcon from '../../../images/flourish-icon-2.png';
+import Popup from 'reactjs-popup';
 
 function Header() {
   return (
     <div className={styles.header}>
       <img className={styles.icon} src={flourishIcon} />
+      
       <div className={styles.headerRight}>
-        <button className={styles.loginButton}>Sign Up / Log In</button>
+        
+        <Popup
+          modal
+          overlayStyle={{ background: 'rgb(255,250,250)' }}
+          closeOnDocumentClick={false}
+          trigger={
+            <button className={styles.loginButton}>Sign Up / Log In</button>
+          }>
+          Signup/Login
+        </Popup>
+
         <svg className={styles.menuIcon} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" /></svg>
       </div>
+
     </div>
   );
 }

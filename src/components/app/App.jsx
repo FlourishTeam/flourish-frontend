022288e5617da42c08831c8529e6a6ea3236ed47
Jillian Gibson <68410308+jillianlg@users.noreply.github.com>
2021-03-05 17:52:24 -0800

    Details/card 2/use details hook (#24)
    * fixes snapshot test
    
    * install react-router-dom, App.jsx snapshot now passes
    
    * starts detials.jsx
    
    * adds props and html to Details.jsx
    
    * imports Details.jsx to DetailsPage.jsx
    
    * adds loading component to DetailPage
    
    * replaces useDetails hook with DetailsContextProvider

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -9,25 +9,28 @@ import SearchPage from '../pages/search/SearchPage';
 import MyCareHistoryPage from '../pages/care-history/MyCareHistoryPage';
 import Header from '../reusable/header/Header';
 import { MyPlantProvider } from '../../state/MyPlantsContext';
+import { DetailsProvider } from '../../state/DetailsContext';
 
 export default function App() {
   return (
     <>
       <Router>
-        <MyPlantProvider>
-          <Header />
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route exact path="/details" component={DetailsPage} />
-            <Route exact path="/my-plants" component={MyPlantsPage} />
-            <Route exact path="/search" component={SearchPage} />
-            <Route
-              exact
-              path="/my-care-history"
-              component={MyCareHistoryPage}
-            />
-          </Switch>
-        </MyPlantProvider>
+        <DetailsProvider>
+          <MyPlantProvider>
+            <Header />
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route exact path="/details" component={DetailsPage} />
+              <Route exact path="/my-plants" component={MyPlantsPage} />
+              <Route exact path="/search" component={SearchPage} />
+              <Route
+                exact
+                path="/my-care-history"
+                component={MyCareHistoryPage}
+              />
+            </Switch>
+          </MyPlantProvider>
+        </DetailsProvider>
       </Router>
     </>
   );
--- src/hooks/useLoading.js
+++ src/hooks/useLoading.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState } from 'react';
 
 export const useLoading = () => {
   const [loading, setLoading] = useState(true);

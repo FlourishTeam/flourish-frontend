20393e87db9fc44e1f15f1580149bf086b25b088
dl-watson <dlarkinwatson@gmail.com>
2021-03-07 15:15:03 -0800

    abstracted photo-upload logic into a custom hook

--- src/components/reusable/search/Search.jsx
+++ src/components/reusable/search/Search.jsx
@@ -1,57 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './styles/Search.css';
 import useSearchResults from '../../../hooks/useSearchResults';
+import usePhotoUploader from '../../../hooks/usePhotoUploader';
 
 const Search = () => {
   const { searchQuery, handleChange } = useSearchResults();
-  const [photoMode, setPhotoMode] = useState(false);
-  const [pictureUpload, setPictureUpload] = useState({});
-  const [lookAlikes, setLookAlikes] = useState([]);
-
-  const API_URL = process.env.REACT_APP_.API_URL;
-  const API_KEY = process.env.REACT_APP_.API_KEY;
-
-  const handlePreview = (e) => {
-    setPictureUpload(URL.createObjectURL(e.target.files[0]));
-  };
-
-  const handleUpload = (e) => {
-    e.preventDefault();
-
-    const files = [...e.target.elements.uploader.files];
-
-    const promise = files.map((file) => {
-      return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.onload = (e) => {
-          const res = e.target.result;
-          resolve(res);
-        };
-        reader.readAsDataURL(file);
-      });
-    });
-
-    Promise.all(promise).then((encodedStr) => {
-      const data = {
-        api_key: `${API_KEY}`,
-        images: encodedStr,
-      };
-
-      fetch(`${API_URL}`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      })
-        .then((res) => res.json())
-        .then((json) => setLookAlikes(json.suggestions));
-    });
-
-    // should trigger a loading spinner while we wait for the fetch to return
-  };
-
-  console.log(lookAlikes);
+  const {
+    photoMode,
+    setPhotoMode,
+    pictureUpload,
+    handleUpload,
+    handlePreview,
+  } = usePhotoUploader();
 
   return (
     <div className={styles.Search}>
---
+++ src/hooks/usePhotoUploader.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+
+const API_URL = process.env.REACT_APP_.API_URL;
+const API_KEY = process.env.REACT_APP_.API_KEY;
+
+const usePhotoUploader = () => {
+  const [photoMode, setPhotoMode] = useState(false);
+  const [pictureUpload, setPictureUpload] = useState({});
+  const [lookAlikes, setLookAlikes] = useState([]);
+
+  const handlePreview = (e) => {
+    setPictureUpload(URL.createObjectURL(e.target.files[0]));
+  };
+
+  const handleUpload = (e) => {
+    e.preventDefault();
+
+    const files = [...e.target.elements.uploader.files];
+
+    const promise = files.map((file) => {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          const res = e.target.result;
+          resolve(res);
+        };
+        reader.readAsDataURL(file);
+      });
+    });
+
+    Promise.all(promise).then((encodedStr) => {
+      const data = {
+        api_key: `${API_KEY}`,
+        images: encodedStr,
+      };
+
+      fetch(`${API_URL}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+        .then((res) => res.json())
+        .then((json) => setLookAlikes(json.suggestions));
+    });
+
+    // should trigger a loading spinner while we wait for the fetch to return
+  };
+
+  console.log(lookAlikes);
+
+  return {
+    photoMode,
+    setPhotoMode,
+    pictureUpload,
+    handleUpload,
+    handlePreview,
+  };
+};
+
+export default usePhotoUploader;

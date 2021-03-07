d2a07324da4c1c3e95463f3a32f282a0a3ec0390
Rhone Lachner <fernandclay@gmail.com>
2021-03-07 14:19:51 -0800

    added initial structure before function is finished

---
+++ src/components/reusable/collection/AddToCollection.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styles from './styles';
+import { setState } from 'react';
+import myPlantList from '../../pages/my-plants/MyPlant';
+import myPlant from '../../pages/my-plants/MyPlant';
+
+export default function AddToCollection() {
+
+  const [myPlantItem, setMyPlantItem] = useState({});
+  //Add myPlant.jsx to MyPlantList.jsx
+
+  const handleClick = () => {
+
+      if(myPlant) {
+    console.log('add button');
+
+    return;
+
+  };
+
+  const handleChange = ({ target }) => {
+    if(target === myPlant) setMyPlantItem(target.value);
+
+
+  };
+
+
+
+  return (
+    <div>
+      <svg className={styles.deleteIcon} handleClick={myPlant} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>     
+    </div>
+  );
+}
+
+

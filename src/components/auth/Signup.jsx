ebbdddc36662e3d59c663f33c9635ba9c9e690e4
Jena Boehm <68626277+jena-boehm@users.noreply.github.com>
2021-03-08 11:12:42 -0800

    Header/card 2/popup login (#31)
    * add services and state folders
    
    * add AuthContext.js
    
    * Add AuthForm.jsx
    
    * add Signup.jsx, Login.jsx, PrivateRoute.jsx
    
    * Add sign up / login button
    
    * add basic popup
    
    * authform rendering in popup. ternary for ssignup/login
    
    * add popup styles
    
    * animation and more styling
    
    * signup working as expected
    
    * auth fully functioning finally
    
    * passing tests
    
    * remove errors
    
    * npm i

--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -1,15 +1,40 @@
-import React from 'react';
-import { useSignup } from '../../state/AuthContext';
+import React, { useState } from 'react';
+import { useLogin, useSignup } from '../../state/AuthContext';
 import AuthForm from './AuthForm';
+import styles from '../reusable/header/styles/Overlay.css';
 
 const Signup = () => {
   const signup = useSignup();
+  const login = useLogin();
+  const [state, setState] = useState('signup');
+
+  const showLogin = () => {
+    setState('login');
+  };
+
+  const showSignup = () => {
+    setState('signup');
+  };
 
   return (
-    <AuthForm 
-      title="Signup" 
-      authFn={signup} 
-    />
+    <div className={styles.popupContainer}> 
+      {
+        state === 'signup' && (
+          <AuthForm 
+            title="Signup" 
+            signupFn={signup} 
+            showLogin={showLogin}
+          />
+        )}
+      {
+        state === 'login' && (
+          <AuthForm 
+            title="Login" 
+            loginFn={login} 
+            showSignup={showSignup}
+          />
+        )}
+    </div>
   );
 };
 

d0813657ea395117b9e621d6de40a577fc4b1832
Rhone Lachner <fernandclay@gmail.com>
2021-03-08 11:36:01 -0800

    added lost changes, updated app route to history

--- src/components/pages/care-history/MyCareHistoryPage.jsx
+++ src/components/pages/care-history/MyCareHistoryPage.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 // import uuid from 'react-uuid';
 import MaintenanceLevel from '../../reusable/maintenance/MaintenanceLevel';
 import CareDetails from '../../reusable/care/CareDetails';
-import CareLogList from '../../reusable/care/CareLog';
+import CareLogList from '../../pages/care-history/CareLogList';
 import CareForm from '../care-history/care-notes/care-form/CareForm';
 import MyPlantNotes from '../care-history/care-notes/MyPlantNotes';
+import Loading from '../../reusable/loading/Loading';
 // import MyPlant from '../my-plants/MyPlant';
 
 // import { useMyPlants } from '../../../state/MyPlantsContext';
@@ -16,14 +17,16 @@ export default function MyCareHistoryPage() {
 
   return (
     <>
-      {/* <li key={uuid()}>
+      {/* <li key={(plant)}>
           <MyPlant 
             name={plant.common_name}
             image={plant.image}
           />
-        // </li> */}
-      {/* // <ul data-testid="plants">{listElement}</ul> */}
+        </li>  */}
+      {/* <ul data-testid="plants">{listElement}</ul> */}
       <div>
+        Hello I am the history Page
+        <Loading />
         <MaintenanceLevel />
         <CareDetails />
         <MyPlantNotes />

d8f4a06677001e7b64c6e62fa90b113690024df1
Rhone Lachner <fernandclay@gmail.com>
2021-03-08 16:23:27 -0800

    adding provider

---
+++ src/state/graphqlContext.jsx
@@ -0,0 +1,14 @@
+import fetch from 'cross-fetch';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: 'https://localhost:7891/graphql',
+  fetch,
+});
+  
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+  
+export default client;

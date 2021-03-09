770a1d0a197cc73d9fb1372a272b077ec196f08f
Rhone Lachner <fernandclay@gmail.com>
2021-03-09 09:33:25 -0800

    updated background colors to view elements more clearly. needs CSS.

--- src/components/reusable/header/NavMenu.jsx
+++ src/components/reusable/header/NavMenu.jsx
@@ -6,68 +6,64 @@ import styles from './styles/NavMenu.css';
 
 
 function NavMenu() {
-  const [sideBar, setSidebar] = useState(false);
+  const [sideMenu, setSideMenu] = useState(false);
     
-  const showSidebar = () => setSidebar(!sideBar);
+  const showSideMenu = () => setSideMenu(!sideMenu);
 
   return (
     <>
       {/* <IconContext.Provider value={{ color: '#fff' }}> */}
-      <div>
-        <div className={styles.navbar}>
+      
+      <div className={styles.navMenu}>
 
-          <Link to="#" className={styles.menuBars} >
-            <svg 
-              onClick={showSidebar} 
-              className={styles.menuIconOpen} 
-              fill="currentColor" 
-              viewBox="0 0 20 20" 
-              xmlns="http://www.w3.org/2000/svg">
-              <path 
-                fillRule="evenodd" 
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" 
-                clipRule="evenodd" />
-            </svg>
-          </Link>    
-        </div>
-        <nav className={styles.sideBar ? 'navMenu active' : 'navMenu'}>
-          <ul className={styles.navMenuItems}>
-            <li className={styles.navBarToggle}>
+        <Link to="#" className={styles.menuBars} >
+          <svg 
+            onClick={showSideMenu} 
+            className={styles.menuIconOpen} 
+            fill="currentColor" 
+            viewBox="0 0 20 20" 
+            xmlns="http://www.w3.org/2000/svg">
+            <path 
+              fillRule="evenodd" 
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" 
+              clipRule="evenodd" />
+          </svg>
+        </Link>    
+      </div>
+      <nav className={styles.sideMenu ? 'styles.navBar active' : 'styles.navBar' }>
+        <ul className={styles.navBarItems}>
+          <li className={styles.navMenuToggle}>
 
-              <Link to="#" className={styles.menuBars}>
-                <svg className={styles.menuIconClose} 
-                  fill="none" 
-                  stroke="currentColor" 
-                  viewBox="0 0 24 24" 
-                  xmlns="http://www.w3.org/2000/svg">
-                  <path strokeLinecap="round" 
-                    strokeLinejoin="round" 
-                    strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                </svg>
-              </Link>
-            </li>
+            <Link to="#" className={styles.menuBars}>
+              <svg className={styles.menuIconClose} 
+                fill="none" 
+                stroke="currentColor" 
+                viewBox="0 0 24 24" 
+                xmlns="http://www.w3.org/2000/svg">
+                <path strokeLinecap="round" 
+                  strokeLinejoin="round" 
+                  strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </Link>
+          </li>
         
 
-            {NavMenuData.map((item, index) => {
-              return (
-                <>
-          
-                  <ul>
-                    <li key={index} className={item.cName} />
-                    <Link to={item.path}>
-                      {item.icon}
-                      <span>{item.title}</span>
-                    </Link>
-                  </ul>
-           
-                </>
-              );
-            })}
+          {NavMenuData.map((item, index) => {
+            return (
+              <>
+                <li key={index} className={item.cName} />
+                <Link to={item.path}>
+                  {item.icon}
+                  <span>{item.title}</span>
+                </Link>
+              </>
+            );
+          })}
 
          
-          </ ul>
-        </nav>
-      </div>
+        </ ul>
+      </nav>
+      
       {/* </IconContext.Provider> */}
     </>
   );

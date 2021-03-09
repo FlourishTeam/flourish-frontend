cf744e6618fb08cf5bcf8c075acb7e1885031428
Rhone Lachner <fernandclay@gmail.com>
2021-03-08 21:22:59 -0800

    added to remove and add functions, commented out and will ask for help tuesday

--- src/components/reusable/collection/AddToCollection.jsx
+++ src/components/reusable/collection/AddToCollection.jsx
@@ -1,34 +1,57 @@
-import React from 'react';
-import styles from './styles';
+// import React from 'react';
+// import styles from './styles';
 // import { useState } from 'react';
-// import myPlantList from '../../pages/my-plants/MyPlant';
 // import myPlant from '../../pages/my-plants/MyPlant';
 
-export default function AddToCollection() {
+// export default function AddToCollection() {
 
-  // const [myPlantItem, setMyPlantItem] = useState({});
-  // //Add myPlant.jsx to MyPlantList.jsx
-
-  // const handleClick = () => {
-
-  //   if(myPlant) {
-  //     console.log('add button');
-
-  //     return;
-
-  //   }
-
-  //   const handleChange = ({ target }) => {
-  //     if(target === myPlant) setMyPlantItem(target.value);
+//   const [myPlantItem, setMyPlantItem] = useState({});
 
 
-  //   };
+//   //DETAILS PAGE FINDS PLANT BT ID
 
 
-
-  return (
-    <div>
-      <svg className={styles.addIcon} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>      </div>
-  );
-}
+// ////////////EXPORT CLICKED TARGET TO ADD PLANT TO USER_PLANTS////////
+//   export async function addUserPlant(token, payload) {
+//     try {
+//         return await request.post(`${herokuBackend}api/userPlants`)
+//         .set('Authorization', token)
+//         .send(payload)
+//     } catch(err) {
+//         throw err;
+//     }
 // }
+// ////////////////////////////////////////////////////////////////////
+
+
+// // clarify myPlants or userPlants front and back end 
+
+// //UPDATE ALL TO MODERN REACT TUESDAY
+
+
+
+//   handleFavorite = async(e) => {
+//     e.preventDefault();
+//     const { name, image } = this.state.plants;
+//     const userPlant = {
+//       name,
+//       image,
+//     };
+//     console.log(this.props.token);
+//     await addToCollection(this.props.token, userPlant);
+//   };
+
+  
+
+//   return (
+//     <div>
+//       <button onClick={this.handleUserPlant} className="addToCollectionButton">
+//                                             Save Plant to Collection
+      
+//         <svg className={styles.addIcon} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+
+//       </button>      
+//     </div>
+//   );
+// }
+
--- src/components/reusable/collection/RemoveFromCollection.jsx
+++ src/components/reusable/collection/RemoveFromCollection.jsx
@@ -1,16 +1,39 @@
-import React from 'react';
-import styles from './styles';
+// import React from 'react';
+// import styles from './styles';
 
-function RemoveFromCollection() {
+// function RemoveFromCollection() {
 
 
-  //Remove myPlantList
+// ////////////////REMOVE PLANT FROM USER_PLANTS//////////////////
+//   export async function deleteUserPlant(someId, token) {
+//     try {
+//         return await request.delete(`${herokuBackend}api/userPlants/${someId}`)
+//         .set('Authorization', token)
+//     } catch(err) {
+//         throw err;
+//     }
+// }
+// ////////////////////////////////////////////////////////////////
 
-  return (
-    <div>
-      <svg className={styles.deleteIcon} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>     
-    </div>
-  );
-}
+// //UPDATE ALL TO MODERN REACT TUESDAY
 
-export default RemoveFromCollection;
+
+// handleDelete = async (someId) => {
+//   await deleteUserPlant(someId, this.props.token);
+//   const response = await fetchAllUserPlants(this.props.token)
+//   await this.setState({ collectionPlants: response.body });
+// }
+
+//   return (
+//     <div>
+
+   
+// <button className="trash" imagevalue={fave.id} onClick={() => this.handleDelete(fave.id)}>
+//    Delete this Plant from Collection</button>
+
+//       <svg className={styles.deleteIcon} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>     
+//     </div>
+//   );
+// }
+
+// export default RemoveFromCollection;

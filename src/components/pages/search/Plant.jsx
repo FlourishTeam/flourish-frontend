3c4767165a66d22acfe696038138d7485bf4bb87
Jena Boehm <68626277+jena-boehm@users.noreply.github.com>
2021-03-09 09:39:14 -0800

    My plant notes/card 1/add form (#41)
    * add services and state folders
    
    * add AuthContext.js
    
    * Add AuthForm.jsx
    
    * add Signup.jsx, Login.jsx, PrivateRoute.jsx
    
    * Add sign up / login button
    
    * add basic popup
    
    * authform rendering in popup. ternary for ssignup/login
    
    * add popup styles
    
    * animation and more styling
    
    * signup working as expected
    
    * auth fully functioning finally
    
    * passing tests
    
    * remove errors
    
    * npm i
    
    * frontend data needs
    
    * write if/else logic
    
    * maintenance level icons rendering on search page
    
    * fix some styling
    
    * committing progress so far
    
    * add care form
    
    * add plant notes form

--- src/components/pages/search/Plant.jsx
+++ src/components/pages/search/Plant.jsx
@@ -4,12 +4,12 @@ import styles from './styles/Plant.css';
 import MaintenanceLevel from '../../reusable/maintenance/MaintenanceLevel';
 
 const Plant = ({
-  common_name,
-  scientific_name,
-  light_range,
-  hydration_range,
-  care_difficulty,
-  temperature_range,
+  commonName,
+  scientificName,
+  lightRange,
+  hydrationRange,
+  careDifficulty,
+  temperatureRange,
 }) => {
   return (
     <div className={styles.Plant}>
@@ -17,23 +17,23 @@ const Plant = ({
         src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fa%2Fad%2FPlaceholder_no_text.svg%2F1200px-Placeholder_no_text.svg.png&f=1&nofb=1"
         height="100px"
       />
-      <div>{common_name}</div>
-      <div className={styles.scientific}>{scientific_name}</div>
-      <div>{temperature_range}</div>
-      <div>{light_range}</div>
-      <div>{hydration_range}</div>
-      <MaintenanceLevel maintenanceLevel={care_difficulty} />
+      <div>{commonName}</div>
+      <div className={styles.scientific}>{scientificName}</div>
+      <div>{temperatureRange}</div>
+      <div>{lightRange}</div>
+      <div>{hydrationRange}</div>
+      <MaintenanceLevel maintenanceLevel={careDifficulty} />
     </div>
   );
 };
 
 Plant.propTypes = {
-  common_name: PropTypes.string.isRequired,
-  scientific_name: PropTypes.string.isRequired,
-  light_range: PropTypes.string.isRequired,
-  hydration_range: PropTypes.string.isRequired,
-  care_difficulty: PropTypes.string.isRequired,
-  temperature_range: PropTypes.string.isRequired,
+  commonName: PropTypes.string.isRequired,
+  scientificName: PropTypes.string.isRequired,
+  lightRange: PropTypes.string.isRequired,
+  hydrationRange: PropTypes.string.isRequired,
+  careDifficulty: PropTypes.string.isRequired,
+  temperatureRange: PropTypes.string.isRequired,
 };
 
 export default Plant;
--- src/hooks/useSearchResults.js
+++ src/hooks/useSearchResults.js
@@ -1,23 +1,44 @@
 import React, { useState } from 'react';
-import { data } from './fake-data';
+import client from '../state/graphqlContext';
+import { gql } from 'apollo-boost';
 
 const useSearchResults = () => {
   const [loading, setLoading] = useState(true);
   // pulling in static data for now, so we can test out rendering in the Search Page
-  const [searchResults, setSearchResults] = useState(data);
+  const [searchResults, setSearchResults] = useState([]);
 
   //searchQuery
   const [searchQuery, setSearchQuery] = useState('');
 
   //handleSearch
+  const handleSearch = (name) => {
+    client
+      .query({
+        query: gql`
+        query {
+          plantByName(name: ${name}) {
+          plantId,
+          image,
+          commonName,
+          scientificName,
+          lightRange,
+          hydrationRange,
+          careDifficulty,
+          temperatureRange
+          }
+        }
+        `,
+      })
+      .then((plant) => setSearchResults(plant));
+  };
 
   //onChange
   const handleChange = ({ target }) => {
     setSearchQuery(target.value);
-    console.log(searchQuery);
+    console.log('SEARCH QUERY', searchQuery);
   };
 
-  return { loading, searchResults, searchQuery, handleChange };
+  return { loading, searchResults, searchQuery, handleChange, handleSearch };
 };
 
 export default useSearchResults;

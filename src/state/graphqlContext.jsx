c26515f2d527e42e65a2d8519d0e86253331770d
Jena Boehm <boehmjena@gmail.com>
2021-03-08 21:06:08 -0800

    committing progress so far

--- src/state/graphqlContext.jsx
+++ src/state/graphqlContext.jsx
@@ -2,9 +2,10 @@ import fetch from 'cross-fetch';
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 
 const httpLink = createHttpLink({
-  uri: 'https://localhost:7891/graphql',
+  uri: 'https://flourish-dev-staging.herokuapp.com/graphql',
   fetch,
 });
+
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache(),

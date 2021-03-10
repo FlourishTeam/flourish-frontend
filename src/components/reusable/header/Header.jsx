70640688a02cf50a94402fb385acb71eca75d8fc
dl-watson <dlarkinwatson@gmail.com>
2021-03-10 13:22:13 -0800

    moved navmenu into its own dir

--- src/components/reusable/header/Header.jsx
+++ src/components/reusable/header/Header.jsx
@@ -1,33 +1,34 @@
 import React from 'react';
 import styles from './styles/Header.css';
 import flourishIcon from '../../../images/flourish-icon-2.png';
-import NavMenu from './NavMenu.jsx';
 import Popup from 'reactjs-popup';
 import Signup from '../../auth/Signup';
+import NavMenu from '../navmenu/NavMenu';
 
 function Header() {
-
   return (
     <div className={styles.header}>
       <img className={styles.icon} src={flourishIcon} />
       <NavMenu />
-      
+
       <div className={styles.headerRight}>
         <Popup
           modal
           closeOnDocumentClick={true}
           // overlayStyle={{ background: 'white', opacity: '0.5' }}
-          trigger={open => <button open={open} className={styles.loginButton}>Sign Up / Log In</button>
-          }>
-          { <Signup open={open} />}
+          trigger={(open) => (
+            <button open={open} className={styles.loginButton}>
+              Sign Up / Log In
+            </button>
+          )}
+        >
+          {<Signup open={open} />}
         </Popup>
-        
+
         {/* <svg className={styles.menuIcon} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" /></svg> */}
       </div>
-
     </div>
   );
 }
 
 export default Header;
-
--- src/components/reusable/navmenu/NavMenu.jsx
+++ src/components/reusable/navmenu/NavMenu.jsx
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
-import { Link }  from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
 import { NavMenuData } from './NavMenuData';
 import styles from './styles/NavMenu.css';
 import { IconContext } from 'react-icons';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 
-
 function NavMenu() {
   const [sideMenu, setSideMenu] = useState(true);
-    
+
   const showSideMenu = () => {
     setSideMenu(!sideMenu);
     console.log('click');
@@ -18,24 +17,18 @@ function NavMenu() {
   return (
     <>
       <IconContext.Provider value={{ color: '#fff' }}>
-      
         <div className={styles.navMenu}>
-
-          <Link to="#" className={styles.menuBars} >
+          <Link to="#" className={styles.menuBars}>
             <FaIcons.FaBars onClick={showSideMenu} />
-          </Link>    
+          </Link>
         </div>
-        <nav className={sideMenu ? styles.menuBars.active : styles.menuBars }>
-        
-        
+        <nav className={sideMenu ? styles.menuBars.active : styles.menuBars}>
           <ul className={styles.navBarItems} onClick={showSideMenu}>
             <li className={styles.navMenuToggle}>
-
               <Link to="#" className={styles.menuBars}>
                 <AiIcons.AiOutlineClose />
               </Link>
             </li>
-        
 
             {NavMenuData.map((item, index) => {
               return (
@@ -48,11 +41,8 @@ function NavMenu() {
                 </>
               );
             })}
-
-         
-          </ ul>
+          </ul>
         </nav>
-      
       </IconContext.Provider>
     </>
   );

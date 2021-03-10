da388fd203608eb30d9d2f91b591d27c6dcda326
Rhone Lachner <fernandclay@gmail.com>
2021-03-10 10:13:59 -0800

    removed unused files, restored header

--- src/components/reusable/header/NavMenu.jsx
+++ src/components/reusable/header/NavMenu.jsx
@@ -1,61 +1,61 @@
-// import React, { useState } from 'react';
-// import { Link }  from 'react-router-dom'; 
-// import { NavMenuData } from './NavMenuData';
-// import styles from './styles/NavMenu.css';
-// import { IconContext } from 'react-icons';
-// import * as FaIcons from 'react-icons/fa';
-// import * as AiIcons from 'react-icons/ai';
+import React, { useState } from 'react';
+import { Link }  from 'react-router-dom'; 
+import { NavMenuData } from './NavMenuData';
+import styles from './styles/NavMenu.css';
+import { IconContext } from 'react-icons';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
 
 
-// function NavMenu() {
-//   const [sideMenu, setSideMenu] = useState(true);
+function NavMenu() {
+  const [sideMenu, setSideMenu] = useState(true);
     
-//   const showSideMenu = () => {
-//     setSideMenu(!sideMenu);
-//     console.log('click');
-//   };
+  const showSideMenu = () => {
+    setSideMenu(!sideMenu);
+    console.log('click');
+  };
 
-//   return (
-//     <>
-//       <IconContext.Provider value={{ color: '#fff' }}>
+  return (
+    <>
+      <IconContext.Provider value={{ color: '#fff' }}>
       
-//         <div className={styles.navMenu}>
+        <div className={styles.navMenu}>
 
-//           <Link to="#" className={styles.menuBars} >
-//             <FaIcons.FaBars onClick={showSideMenu} />
-//           </Link>    
-//         </div>
-//         <nav className={sideMenu ? styles.menuBars.active : styles.menuBars }>
+          <Link to="#" className={styles.menuBars} >
+            <FaIcons.FaBars onClick={showSideMenu} />
+          </Link>    
+        </div>
+        <nav className={sideMenu ? styles.menuBars.active : styles.menuBars }>
         
         
-//           <ul className={styles.navBarItems} onClick={showSideMenu}>
-//             <li className={styles.navMenuToggle}>
+          <ul className={styles.navBarItems} onClick={showSideMenu}>
+            <li className={styles.navMenuToggle}>
 
-//               <Link to="#" className={styles.menuBars}>
-//                 <AiIcons.AiOutlineClose />
-//               </Link>
-//             </li>
+              <Link to="#" className={styles.menuBars}>
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
         
 
-//             {NavMenuData.map((item, index) => {
-//               return (
-//                 <>
-//                   <li key={index} className={item.cName} />
-//                   <Link to={item.path}>
-//                     {item.icon}
-//                     <span>{item.title}</span>
-//                   </Link>
-//                 </>
-//               );
-//             })}
+            {NavMenuData.map((item, index) => {
+              return (
+                <>
+                  <li key={index} className={item.cName} />
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </>
+              );
+            })}
 
          
-//           </ ul>
-//         </nav>
+          </ ul>
+        </nav>
       
-//       </IconContext.Provider>
-//     </>
-//   );
-// }
+      </IconContext.Provider>
+    </>
+  );
+}
 
-// export default NavMenu;
+export default NavMenu;

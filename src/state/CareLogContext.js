06d61035233b2114d729379c7d6b0e85f121e6a6
jeffIrvine <jirvine1212@gmail.com>
2021-03-10 15:28:18 -0800

    built CareLogContext

--- src/state/CareLogContext.js
+++ src/state/CareLogContext.js
@@ -1,18 +1,103 @@
 import React, { createContext, useContext, useState } from 'react';
-
+import client from './GraphQLContext';
+import { gql } from 'apollo-boost';
 
 export const CareLogContext = createContext(null);
 
 export const CareLogProvider = ({ children }) => {
-  const [careLogItems, setCareLog] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [plant, setPlant] = useState({});
+  const [careLogItems, setCareLog] = useState(null);
+  const [error, setError] = useState(null);
+
+  const renderMyCareHistory = (userId, plantId) => {
+    setError(null);
+    setLoading(true);
+
+    client
+      .query({
+        query: gql`
+        query {
+          getMyCareHistoryById(plantId: ${plantId}, userId: ${userId}) {
+            careLogs {
+              userPlantLogId
+              userId
+              plantId
+              userPlantId
+              careDate
+              careType
+              careNote
+            }
+            plantDetails {
+              plantId
+              image
+              commonName
+              synonyms
+              scientificName
+              lightRange
+              hydrationRange
+              careDifficulty
+              pestsDiseases
+              warnings
+              height
+              spread
+              type
+              floweringPeriod
+              bloomSize
+              temperatureRange
+              placement
+              substrateRecommendation
+              pottingNotes
+              watering
+              propagation
+            }
+          }
+        }
+      `, })
+      .then(({ data }) => {
+        setCareLog(data.getMyCareHistoryById);
+        setPlant(data.plantDetails);
+        setLoading(false);
+
+      })
+      .catch((error) => setError(error.message));
+  };
+
 
   return (
-    <CareLogContext.Provider value={{ careLogItems }}>
+    <CareLogContext.Provider value={{ 
+      loading,
+      careLogItems,
+      renderMyCareHistory,
+      plant,
+      error 
+    }}>
       {children}
     </CareLogContext.Provider>
   );
 };
 
+export const useCareLogLoading = () => {
+  const { loading } = useContext(CareLogContext);
+  return loading;
+};
+
 export const useCareLogItems = () => {
-  return useContext(CareLogContext);
+  const { careLogItems } = useContext(CareLogContext);
+  return careLogItems;
+};
+
+export const usePlant = () => {
+  const { plant } = useContext(CareLogContext);
+  return plant;
+};
+
+export const useCareLogError = () => {
+  const { error } = useContext(CareLogContext);
+  return error;
+};
+
+export const useRenderMyCareHistory = () => {
+  const { renderMyCareHistory } = useContext(CareLogContext);
+  return renderMyCareHistory;
 };

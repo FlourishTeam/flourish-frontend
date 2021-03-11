11b9019f8b785931dcf73e6b319ca5e6e0b8c0b1
Rhone Lachner <fernandclay@gmail.com>
2021-03-11 11:02:11 -0800

    further conflict fixes

--- src/components/reusable/collection/RemoveFromCollection.jsx
+++ src/components/reusable/collection/RemoveFromCollection.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import PropTypes from "prop-types";
 import { removePlantFromCollection } from '../../../services/fetchRemovePlant';
 import { useSession } from '../../../state/AuthContext';
 import styles from './styles/AddToCollection.css';

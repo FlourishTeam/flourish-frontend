b171ca09b72fa0ba1e2778fa173cb9b0fdde71ce
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-03-11 13:20:52 -0800

    adds aboutUsPage, sets up file framework

---
+++ src/components/pages/about-us/AboutUs.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './styles/AboutUsPage.css';
+
+
+export default function AboutUs() {
+  return (
+    <div className={styles.AboutUs}>
+      <section className={styles.bio}>
+        <img src=" " alt=" " className={image} width="100%" height="90%" />
+        <span className={styles.name}></span>
+        <span className={styles.title}></span>
+        <span className={styles.tbd}></span>
+        <span className={styles.tbd}></span>
+        <span className={styles.tbd}></span>
+      </section>
+    </div>
+  );
+}
+
+

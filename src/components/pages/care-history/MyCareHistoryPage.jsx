5d2105b309ff336c2f6c5e56a254225b3cd6d159
Jeff Irvine (he/him) <68131101+jeffIrvine@users.noreply.github.com>
2021-03-11 15:23:23 -0800

    My care history/ card 3 build query (#67)
    * built my plant item and added snapshot testing
    
    * built my plant list along with snapshot testing
    
    * end of day building plant page
    
    * built myplant, myplantlist, myplantpage with snapshot testing
    
    * github ci failing in packagejson
    
    * fixed pathing in appjs
    
    * updated snapshot
    
    * removed page from history path
    
    * wrote some of the test, stopped to wait for graphql
    
    * removed nested objects from json data
    
    * changes did not merge
    
    * built careLogItem, careLogList, CareLogContext, added provider around appjs
    
    * fixed pathing typo
    
    * merging from pull
    
    * built CareLogContext
    
    * built out mycarehistory page and set hooks for careDetails and plant info
    
    * merging
    
    * commiting before pulling
    
    * pulling for fresh code
    
    * working rendering but some functionality still missing

--- src/components/pages/care-history/MyCareHistoryPage.jsx
+++ src/components/pages/care-history/MyCareHistoryPage.jsx
@@ -1,35 +1,63 @@
-// import React from 'react';
-// // import uuid from 'react-uuid';
-// import MaintenanceLevel from '../../reusable/maintenance/MaintenanceLevel';
-// // import CareDetails from '../../reusable/care/CareDetails';
-// import CareLogList from '../../pages/care-history/CareLogList';
-// import CareForm from '../care-history/care-notes/care-form/CareForm';
-// import MyPlantNotes from '../care-history/care-notes/MyPlantNotes';
-// import Loading from '../../reusable/loading/Loading';
-// // import MyPlant from '../my-plants/MyPlant';
-// // import { useMyPlants } from '../../../providers/MyPlantsContext';
+import React, { useEffect } from 'react';
+import { 
+  usePlantDetails, 
+  useCareLogError, 
+  useCareLogLoading,
+  useRenderMyCareHistory,
+} from '../../../providers/CareLogContext';
+import MaintenanceLevel from '../../reusable/maintenance/MaintenanceLevel';
+import CareForm from '../care-history/care-notes/care-form/CareForm';
+import CareLogList from '../../pages/care-history/CareLogList';
+import { useSession } from '../../../providers/AuthContext';
+import Loading from '../../reusable/loading/Loading';
+import styles from './styles/MyCareHistory.css';
+import { useParams } from 'react-router';
 
-// // const { myPlants } = useMyPlants();
-  
-// return (
-//   <>
-//     {/* <li key={(plant)}>
-//           <MyPlant 
-//             name={plant.common_name}
-//             image={plant.image}
-//           />
-//         </li>  */}
-//     {/* <ul data-testid="plants">{listElement}</ul> */}
-//     <div>
-//         Hello I am the history Page
-//       <Loading />
-//       <MaintenanceLevel maintenanceLevel={maintenanceLevel} />
-//       {/* <CareDetails /> */}
-//       <MyPlantNotes />
-//       <CareForm />
-//       <CareLogList />
-//     </div>
-//   </>
-// );
-// // });
+export default function MyCareHistoryPage() {
+  const plantDetails = usePlantDetails();
+  const loading = useCareLogLoading();
+  const error = useCareLogError();
+  const { id } = useParams();
+  const user = useSession();
+  const renderMyCareHistory = useRenderMyCareHistory();
+  useEffect(() => {
+    renderMyCareHistory(user.id, id);
+  }, []);
 
+  return (
+    <>
+      {error &&  
+      <div>
+        <CareForm />
+        {error}
+      </div>
+      }
+      {loading && <Loading />}
+      <p>Hello!</p>
+      <div className={styles.MyCareHistoryPlantName}>
+        <div>{plantDetails.commonName}</div>
+        <div>{plantDetails.scientificName}</div>
+        <img className={styles.MyCareHistoryImage}
+          src={plantDetails.image} 
+          alt={plantDetails.commonName} 
+        />
+        <div>Maintenance Level: 
+          <MaintenanceLevel maintenanceLevel={plantDetails.careDifficulty}/>
+        </div>
+        {/* <RemoveFromCollection />  */}
+          CARE DETAILS SECTION */
+        <section>
+          <div> Light Range: {plantDetails.lightRange}</div>
+          <div> Hydration Range: {plantDetails.hydrationRange}</div>
+          <div> Temperature Range: {plantDetails.temperatureRange}</div>
+          <div> Placement: {plantDetails.placement}</div>
+          <div> Substrate Recommendation: {plantDetails.substrateRecommendation}</div>
+          <div> Potting Notes: {plantDetails.pottingNotes}</div>
+          <div> Watering: {plantDetails.watering}</div>
+        </section>  
+      </div>
+      <CareForm />
+      <CareLogList />  
+    </>
+  );
+}

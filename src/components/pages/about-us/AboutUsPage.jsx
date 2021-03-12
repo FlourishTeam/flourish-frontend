cba9a815be4e01fb3a7a662fa875f0868837c412
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-03-11 19:12:11 -0800

    add basic css to about us page

--- src/components/pages/about-us/AboutUsPage.jsx
+++ src/components/pages/about-us/AboutUsPage.jsx
@@ -9,68 +9,103 @@ import styles from './styles/AboutUsPage.css';
 
 export default function AboutUs() {
   return (
-    <div className={styles.AboutUs}>
-      <section className={styles.bio}>
-        <img src={DeeImage} alt="Image of Dee" className={image} width="100%" height="90%" />
-        <span className={styles.name}>Dee Watson</span>
-        <span className={styles.title}>Just a Dude</span>
-        <h4 className={styles.heading}>Plant Experience</h4>
-        <span className={styles.skill_key}>Skill Level:</span>
-        <span className={styles.skill_value}>Tries really hard.</span>
-        <span className={styles.plant_key}>Number of Plants:</span>
-        <span className={styles.plant_value}>5???</span>
-        <span className={styles.quote_key}>Quote:</span>
-        <span className={styles.quote_value}>A plant is just a little guy who is your friend.</span>
-      </section>
-      <section className={styles.bio}>
-        <img src={JillianImage} alt="Image of Jillian" className={image} width="100%" height="90%" />
-        <span className={styles.name}>Jillian Gibson</span>
-        <span className={styles.title}>Software Engineer</span>
-        <h4 className={styles.heading}>Plant Experience</h4>
-        <span className={styles.skill_key}>Skill Level:</span>
-        <span className={styles.skill_value}>Novice</span>
-        <span className={styles.plant_key}>Number of Plants:</span>
-        <span className={styles.plant_value}>0</span>
-        <span className={styles.quote_key}>Quote:</span>
-        <span className={styles.quote_value}>I can't even keep bamboo alive.</span>
-      </section>
-      <section className={styles.bio}>
-        <img src={JeffImage} alt="Image of Jeff" className={image} width="100%" height="90%" />
-        <span className={styles.name}>Jeff Irvine</span>
-        <span className={styles.title}>Software Engineer</span>
-        <h4 className={styles.heading}>Plant Experience</h4>
-        <span className={styles.skill_key}>Skill Level:</span>
-        <span className={styles.skill_value}>Beginner</span>
-        <span className={styles.plant_key}>Number of Plants:</span>
-        <span className={styles.plant_value}>???</span>
-        <span className={styles.quote_key}>Quote:</span>
-        <span className={styles.quote_value}>Baby has a new leaf!</span>
-      </section>
-      <section className={styles.bio}>
-        <img src={JenaImage} alt="Image of Jena" className={image} width="100%" height="90%" />
-        <span className={styles.name}>Jena Boehm</span>
-        <span className={styles.title}>Software Engineer and Plant EnthusiastPlant</span>
-        <h4 className={styles.heading}>Plant Experience</h4>
-        <span className={styles.skill_key}>Skill Level:</span>
-        <span className={styles.skill_value}>I don't know man, just doing my best.</span>
-        <span className={styles.plant_key}>Number of Plants:</span>
-        <span className={styles.plant_value}>34</span>
-        <span className={styles.quote_key}>Quote:</span>
-        <span className={styles.quote_value}>I try really hard not to be *that* plant person, but I think I'm kinda turning into exactly that. Oops.</span>
-      </section>
-      <section className={styles.bio}>
-        <img src={RhoneImage} alt="Image of Rhone" className={image} width="100%" height="90%" />
-        <span className={styles.name}>Rhone </span>
-        <span className={styles.title}></span>
-        <h4 className={styles.heading}>Plant Experience</h4>
-        <span className={styles.skill_key}>Skill Level:</span>
-        <span className={styles.skill_value}></span>
-        <span className={styles.plant_key}>Number of Plants:</span>
-        <span className={styles.plant_value}></span>
-        <span className={styles.quote_key}>Quote:</span>
-        <span className={styles.quote_value}></span>
-      </section>
-    </div>
+    <>
+      <div className={styles.AboutUs}>
+        <h3 className={styles.header}>Team Flourish</h3>
+        <div className={styles.team_bios}>
+          <section className={styles.bio}>
+            <img src={DeeImage} alt="Image of Dee" className={styles.image} width="200px" />
+            <span className={styles.name}>Dee Watson</span>
+            <span className={styles.title}>Just a Dude</span>
+            <h5 className={styles.heading}>Plant Experience</h5>
+            <div className={styles.data}>
+              <span className={styles.key}>Skill Level:</span>
+              <span>Tries really hard.</span>
+            </div>
+            <div className={styles.data}>
+              <span className={styles.key}>Number of Plants:</span>
+              <span>5???</span>
+            </div>
+            <div className={styles.data}>
+              <span className={styles.key}>Quote:</span>
+              <span>A plant is just a little guy who is your friend.</span>
+            </div>
+          </section>
+          <section className={styles.bio}>
+            <img src={JillianImage} alt="Image of Jillian" className={styles.image} width="200px" />
+            <span className={styles.name}>Jillian Gibson</span>
+            <span className={styles.title}>Software Engineer</span>
+            <h5 className={styles.heading}>Plant Experience</h5>
+            <div className={styles.data}>
+              <span className={styles.key}>Skill Level:</span>
+              <span>Novice</span>
+            </div>
+            <div className={styles.data}>
+              <span className={styles.key}>Number of Plants:</span>
+              <span>0</span>          
+            </div>
+            <div className={styles.data}>
+              <span className={styles.key}>Quote:</span>
+              <span>I can't even keep bamboo alive.</span>
+            </div>
+          </section>
+          <section className={styles.bio}>
+            <img src={JeffImage} alt="Image of Jeff" className={styles.image} width="200px"/>
+            <span className={styles.name}>Jeff Irvine</span>
+            <span className={styles.title}>Software Engineer</span>
+            <h5 className={styles.heading}>Plant Experience</h5>
+            <div className={styles.data}>
+              <span className={styles.key}>Skill Level:</span>
+              <span>Beginner</span>
+            </div>
+            <div className={styles.data}>
+              <span className={styles.key}>Number of Plants:</span>
+              <span>???</span>
+            </div>
+            <div className={styles.data}>
+              <span className={styles.key}>Quote:</span>
+              <span>Baby has a new leaf!</span>
+            </div>
+          </section>
+          <section className={styles.bio}>
+            <img src={JenaImage} alt="Image of Jena" className={styles.image} width="200px"/>
+            <span className={styles.name}>Jena Boehm</span>
+            <span className={styles.title}>Software Engineer and Plant Enthusiast</span>
+            <h5 className={styles.heading}>Plant Experience</h5>
+            <div className={styles.data}>
+              <span className={styles.key}>Skill Level:</span>
+              <span>I don't know man, just doing my best.</span>
+            </div>
+            <div className={styles.data}>
+              <span className={styles.key}>Number of Plants:</span>
+              <span>34</span>
+            </div>
+            <div className={styles.data}>
+              <span className={styles.key}>Quote:</span>
+              <span>I try really hard not to be *that* plant person, but I think I'm kinda turning into exactly that. Oops.</span>
+            </div>
+          </section>
+          <section className={styles.bio}>
+            <img src={RhoneImage} alt="Image of Rhone" className={styles.image} width="200px"/>
+            <span className={styles.name}>Rhone </span>
+            <span className={styles.title}>Software Engineer and Nature Enthusiast</span>
+            <h5 className={styles.heading}>Plant Experience</h5>
+            <div className={styles.data}>
+              <span className={styles.key}>Skill Level:</span>
+              <span>Human-plant hybrid person</span>
+            </div>
+            <div className={styles.data}>
+              <span className={styles.key}>Number of Plants:</span>
+              <span>They own themselves, but I have many plant friends.</span>
+            </div>
+            <div className={styles.data}>
+              <span className={styles.key}>Quote:</span>
+              <span>All good things are wild and free.</span>
+            </div>
+          </section>
+        </div>
+      </div>
+    </>
   );
 }
 

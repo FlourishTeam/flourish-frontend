1fb7d4479b1d585afee9f634fb719ea62ead4c57
Jena Boehm <68626277+jena-boehm@users.noreply.github.com>
2021-03-12 09:19:45 -0800

    Auth/private routes (#92)
    * add services and state folders
    
    * add AuthContext.js
    
    * Add AuthForm.jsx
    
    * add Signup.jsx, Login.jsx, PrivateRoute.jsx
    
    * Add sign up / login button
    
    * add basic popup
    
    * authform rendering in popup. ternary for ssignup/login
    
    * add popup styles
    
    * animation and more styling
    
    * signup working as expected
    
    * auth fully functioning finally
    
    * passing tests
    
    * remove errors
    
    * npm i
    
    * frontend data needs
    
    * write if/else logic
    
    * maintenance level icons rendering on search page
    
    * fix some styling
    
    * committing progress so far
    
    * add deleteUser in AuthContext
    
    * merge conflicts
    
    * logout working
    
    * error handling rendering correctly
    
    * homepage, header styling final touches
    
    * one more thing
    
    * details page styling
    
    * styles
    
    * WOOOOO
    
    * fix tests
    
    * things
    
    * fix
    
    * button hover/active styling, little touchups
    
    * more touchups
    
    * styling touchups
    
    * add private routes & conditionally render save to collection
    
    * auth finishing touches

--- src/components/reusable/navmenu/NavMenu.jsx
+++ src/components/reusable/navmenu/NavMenu.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { slide as Menu } from 'react-burger-menu';
-import { useLogout } from '../../../providers/AuthContext';
+import { useLogout, useSession } from '../../../providers/AuthContext';
 import { menuStyle } from './styles/menuStyle';
 import styles from './styles/NavMenu.css';
 
 const NavMenu = () => {
   const logout = useLogout();
+  const user = useSession();
   const [isOpen, setIsOpen] = useState(null);
 
   const handleDelete = (e) => {
@@ -15,28 +16,52 @@ const NavMenu = () => {
   };
 
   return (
-    <Menu
-      className={styles.NavMenu}
-      styles={menuStyle}
-      right={true}
-      isOpen={isOpen}
-    >
-      <a id="home" className="menu-item" href="/">
-        Home
-      </a>
-      <a id="search" className="menu-item" href="/search">
-        Search Flourish Library
-      </a>
-      <a id="my-plants" className="menu-item" href="/my-plants">
-        My Plants
-      </a>
-      <a id="about-us" className="menu-item" href="/about-us">
-        About Us
-      </a>
-      <a id="sign-out" className="menu-item" onClick={handleDelete} href="/">
-        Log Out
-      </a>
-    </Menu>
+    <>
+      {user
+        ? (
+          <>
+            <Menu
+              className={styles.NavMenu}
+              styles={menuStyle}
+              right={true}
+              isOpen={isOpen}>
+              <a id="home" className="menu-item" href="/">
+            Home
+              </a>
+              <a id="search" className="menu-item" href="/search">
+            Search Flourish Library
+              </a>
+              <a id="my-plants" className="menu-item" href="/my-plants">
+            My Plants
+              </a>
+              <a id="about-us" className="menu-item" href="/about-us">
+            About Us
+              </a>
+              <a id="sign-out" className="menu-item" onClick={handleDelete} href="/">
+            Log Out
+              </a>
+            </Menu>
+          </>
+        ) : (
+          <>
+            <Menu
+              className={styles.NavMenu}
+              styles={menuStyle}
+              right={true}
+              isOpen={isOpen}>
+              <a id="home" className="menu-item" href="/">
+            Home
+              </a>
+              <a id="search" className="menu-item" href="/search">
+            Search Flourish Library
+              </a>
+              <a id="about-us" className="menu-item" href="/about-us">
+            About Us
+              </a>
+            </Menu>
+          </>
+        )}
+    </>
   );
 };
 

ffe61eb00aaa75ef85988525f4bd9a61e7078af0
Jena Boehm <68626277+jena-boehm@users.noreply.github.com>
2021-03-11 22:34:14 -0800

    Css/styling details (#79)
    * add services and state folders
    
    * add AuthContext.js
    
    * Add AuthForm.jsx
    
    * add Signup.jsx, Login.jsx, PrivateRoute.jsx
    
    * Add sign up / login button
    
    * add basic popup
    
    * authform rendering in popup. ternary for ssignup/login
    
    * add popup styles
    
    * animation and more styling
    
    * signup working as expected
    
    * auth fully functioning finally
    
    * passing tests
    
    * remove errors
    
    * npm i
    
    * frontend data needs
    
    * write if/else logic
    
    * maintenance level icons rendering on search page
    
    * fix some styling
    
    * committing progress so far
    
    * add deleteUser in AuthContext
    
    * merge conflicts
    
    * logout working
    
    * error handling rendering correctly
    
    * homepage, header styling final touches
    
    * one more thing
    
    * details page styling
    
    * styles
    
    * WOOOOO
    
    * fix tests
    
    * things

--- src/components/reusable/loading/Loading.jsx
+++ src/components/reusable/loading/Loading.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import styles from './styles/Loading.css';
 
 const Loading = () => (
-  <img
-    src="http://www.jainstechnology.com/wp-content/uploads/2018/02/loader.gif"
-    alt="Loading"
-    styles={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}
-  />
+  <div className={styles.loadingContainer}>
+    <img
+      src="http://www.jainstechnology.com/wp-content/uploads/2018/02/loader.gif"
+      alt="Loading"
+      styles={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    />
+  </div>
 );
 
 export default Loading;

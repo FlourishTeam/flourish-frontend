8a4c629491862796b8d5d089ef9811ec5f649f1b
Jena Boehm <boehmjena@gmail.com>
2021-06-21 13:23:23 -0700

    removed extra space on home page and slideshow

--- src/components/pages/home/HomePage.jsx
+++ src/components/pages/home/HomePage.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import styles from './styles/HomePage.css';
 import Search from '../../reusable/search/Search';
-import Slideshow from './Slideshow';
+// import Slideshow from './Slideshow';
 
 const HomePage = () => {
   return (
     <section className={styles.HomePage}>
-      <header className={styles.bannerContainer}>
+      <div className={styles.bannerContainer}>
         <p className={styles.topText}>Welcome to Flourish</p>
-        <Slideshow />
+        {/* <Slideshow /> */}
         <div className={styles.homeSearch}>
           <Search className={styles.Search} />
         </div>
-      </header>
+      </div>
     </section>
   );
 };
